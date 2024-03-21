0e2128f86e21355aab6a3a0799004afaedd24c60
Quan <2200191123@qq.com>
2024-03-22 00:29:08 +0800

    提交初始/临时代码

---
+++ QuanLib.Commands/CommandArgument.cs
@@ -0,0 +1,69 @@
+﻿using QuanLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Commands
+{
+    public class CommandArgument
+    {
+        public CommandArgument(ParameterInfo parameterInfo, int index)
+        {
+            ArgumentNullException.ThrowIfNull(parameterInfo, nameof(parameterInfo));
+            ThrowHelper.ArgumentOutOfMin(0, index, nameof(index));
+
+            Index = index;
+            Type = parameterInfo.ParameterType;
+
+            DisplayAttribute? displayAttribute = parameterInfo.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute is not null)
+            {
+                Name = displayAttribute.Name ?? string.Empty;
+                Description = displayAttribute.Description ?? string.Empty;
+            }
+            else
+            {
+                displayAttribute = parameterInfo.ParameterType.GetCustomAttribute<DisplayAttribute>();
+                if (displayAttribute is not null)
+                {
+                    Name = displayAttribute.Name ?? string.Empty;
+                    Description = displayAttribute.Description ?? string.Empty;
+                }
+                else
+                {
+                    Name = parameterInfo.Name ?? string.Empty;
+                    Description = string.Empty;
+                }
+            }
+
+            Parser = ParserBuilder.FromType(parameterInfo.ParameterType);
+
+            List<ValidationAttribute> validationAttributes = [];
+            IEnumerable<Attribute> attributes = parameterInfo.GetCustomAttributes();
+            foreach (Attribute attribute in attributes)
+            {
+                if (attribute is ValidationAttribute validationAttribute)
+                    validationAttributes.Add(validationAttribute);
+            }
+
+            ValidationAttributes = validationAttributes.AsReadOnly();
+        }
+
+        public int Index { get; }
+
+        public Type Type { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public Parser<object> Parser { get; }
+
+        public ReadOnlyCollection<ValidationAttribute> ValidationAttributes { get; }
+    }
+}
---
+++ QuanLib.Commands/CommandFunction.cs
@@ -0,0 +1,66 @@
+﻿using QuanLib.Commands.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Commands
+{
+    public class CommandFunction
+    {
+        public CommandFunction(object? owner, MethodInfo methodInfo)
+        {
+            ArgumentNullException.ThrowIfNull(methodInfo, nameof(methodInfo));
+
+            List<CommandArgument> arguments = [];
+            ParameterInfo[] parameters = methodInfo.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+                arguments.Add(new(parameters[i], i));
+
+            Arguments = arguments.AsReadOnly();
+            _owner = owner;
+            _methodInfo = methodInfo;
+        }
+
+        private readonly object? _owner;
+
+        private readonly MethodInfo _methodInfo;
+
+        public bool IsStatic => _methodInfo.IsStatic;
+
+        public ReadOnlyCollection<CommandArgument> Arguments { get; }
+
+        public object? Execute(params string[] args)
+        {
+            ArgumentNullException.ThrowIfNull(args, nameof(args));
+
+            if (args.Length != Arguments.Count)
+                throw new CommandArgumentCountException(Arguments.Count, args.Length);
+
+            object[] objects = new object[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                try
+                {
+                    objects[i] = Arguments[i].Parser.Parse(args[i], null);
+                }
+                catch (Exception ex)
+                {
+                    throw new CommandArgumentFormatException(Arguments[i], args[i], null, ex);
+                }
+            }
+
+            try
+            {
+                return _methodInfo.Invoke(_owner, objects);
+            }
+            catch (Exception ex)
+            {
+                throw new CommandException(null, ex);
+            }
+        }
+    }
+}
