7a0893a2553f2252e8358e938dee16eefc5c00e7
Quan <2200191123@qq.com>
2024-04-12 21:38:40 +0800

    调整命令参数验证逻辑

--- QuanLib.Commands/CommandFunctionHelper.cs
+++ QuanLib.Commands/CommandFunctionHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -19,11 +20,22 @@ namespace QuanLib.Commands
             {
                 try
                 {
+
                     result[i] = commandFunction.Arguments[i].Parser.Parse(args[i], null);
+                    foreach (ValidationAttribute validationAttribute in commandFunction.Arguments[i].ValidationAttributes)
+                        validationAttribute.Validate(result[i], commandFunction.Arguments[i].Name);
+                }
+                catch (FormatException ex)
+                {
+                    throw new FormatException($"无法将命令参数“{args[i]}”解析为“{commandFunction.Arguments[i].Type}”类型", ex);
+                }
+                catch (ValidationException ex)
+                {
+                    throw new ValidationException($"命令参数“{args[i]}”的值不是预期的", ex);
                 }
                 catch (Exception ex)
                 {
-                    throw new FormatException($"无法将命令参数“{args[i]}”解析为“{commandFunction.Arguments[i].Type}”类型", ex);
+                    throw new AggregateException($"解析命令参数“{args[i]}”时遇引发了异常", ex);
                 }
             }
 
